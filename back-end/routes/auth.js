b04ddd3af1e88cab778aeb5e21e9814c81a1bb0b
chintan <imchintan0207@gmail.com>
2023-06-21 00:07:06 +0530

    send token completed

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -3,6 +3,9 @@ const User = require('../models/User');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
 // create a user using: POST "api/auth/createuser".no login required
 
 router.post('/createuser', [
@@ -21,31 +24,42 @@ router.post('/createuser', [
 
     //Check whether user with their email exists already
 
- try{
+    try {
 
- 
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "sorry user with this email already exists" })
+        }
+        //secure password by bcryptjs salt
 
-   let user = await User.findOne({email:req.body.email});
-   if (user){
-    return res.status(400).json({error:"sorry user with this email already exists"})
-   } 
+        const salt = await bcrypt.genSalt(10);
+        secPass = await bcrypt.hash(req.body.password, salt)
 
-   //create new user
-   user= await User.create({
-        name: req.body.name,
-        email:req.body.email,
-        password: req.body.password
-      })
-      
+        //create new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        })
+
+        // return token
+
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, "JWT_SECRET");
     
-    res.json(user)
- } 
- //catch the errors
- catch(error){
-    console.error(error.message);
-    res.status(500).send("some error occurred")
+        res.json({authtoken})
+    }
+    //catch the errors
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("some error occurred")
 
- }
+    }
 })
 
 module.exports = router
